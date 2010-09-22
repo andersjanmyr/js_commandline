8c45a78ba83ad50fe069d98529324b71e7cad75e
Anders Janmyr <anders@janmyr.com>
2010-09-22 13:34:07 +0200

    Added error handling

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ enable :run
 set :public, File.dirname(__FILE__) + '/public'
 
 get '/:command' do
-  puts params[:command]
-  `#{params[:command]}`
+  begin
+    `#{params[:command]} 2>&1`
+  rescue Exception => err
+    err.message
+  end
 end
